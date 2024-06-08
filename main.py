1b27e7e00f3309f4fbf775f634298c07e449c609
sonia <soniakataria154@gmail.com>
2024-06-08 13:46:20 +0100

    adding functions to code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pymupdf4llm
-import fitz
+# import fitz
 import pymupdf
 import pymupdf4llm
 import pathlib
@@ -11,3 +11,21 @@ from langchain.text_splitter import MarkdownTextSplitter
 import os
 from openai import OpenAI
 from pinecone import Pinecone
+# defining our project specific variables:
+
+PINECONE_VECTOR_DB_NAME = 'viral_new'
+# ---------------------------------
+pc = Pinecone(api_key=os.environ.get('PINECONE_API_KEY'))
+index = pc.Index(PINECONE_VECTOR_DB_NAME)
+client = OpenAI()
+
+def get_embeddings(text, model = "text-embedding-ada-002"):
+  text = text.replace("\n"," ")
+  return client.embeddings.create(input = text, model = model).data[0].embedding
+
+def get_context(query, embed_model='text-embedding-ada-002', k=5):
+  query_embeddings = get_embeddings(query,model=embed_model)
+  pinecone_res = index.query(vector = query_embeddings, top_k = k, include_metadata=True)
+  contexts = [item['metadata']['text'] for item in pinecone_res['matches']]
+  return contexts, query
+
