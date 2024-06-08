52653fd3d01a22bf1a4d35cdcdad5806233060b0
sonia <soniakataria154@gmail.com>
2024-06-08 10:58:18 +0100

    june8

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import torch 
-import clip 
-from PIL import Image 
-import glob 
-import numpy as np 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device = device)
-print(f'all models:{clip.available_models()}')
-#Function for embedding images 
