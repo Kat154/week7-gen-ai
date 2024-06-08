83380ca5884c631fb3f6d12d1dff7eef873fa173
sonia <soniakataria154@gmail.com>
2024-06-08 12:51:10 +0100

    added libraries

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pymupdf4llm
+import fitz
+import pymupdf
+import pymupdf4llm
+import pathlib
+from langchain.text_splitter import MarkdownTextSplitter
+import os
+from openai import OpenAI
+from pinecone import Pinecone
